159182f0127f2b40ed84c1ec5472c74177708358
Kuehar <green95251@gmail.com>
2019-07-15 17:11:14 +0900

    basic design updated

--- myhp/urls.py
+++ myhp/urls.py
@@ -3,5 +3,6 @@ from . import  views
 
 app_name = 'myhp' #django2.0以降ではnamespace定義が必要
 urlpatterns = [
-    path('', views.index, name='index')
+    # path('', views.index, name='index') 一旦コメントアウト
+    path('', views.news, name='news'), #追加要素
 ]
--- myhp/views.py
+++ myhp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
+from .models import Topic
 
 # Create your views here.
 def index(request):
     template = loader.get_template('index.html')
     context = {}
     return HttpResponse(template.render(context,request))
+
+def news(request):
+    queryset = Topic.objects.all()
+    context = { "news":queryset }
+    return render(request, 'news.html', context)
