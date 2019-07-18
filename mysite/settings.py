93816deb144362b530ab07b6733cd6fa675ae80a
Kuehar <green95251@gmail.com>
2019-07-18 22:54:41 +0900

    CSS was Applied and Design was changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
